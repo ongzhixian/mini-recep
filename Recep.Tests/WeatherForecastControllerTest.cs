6d0c73df735fa07e802f1d7e4a283502f9af7631
Zhixian Ong <zhixian@hotmail.com>
2022-01-25 13:39:03 +0800

    update

---
+++ Recep.Tests/WeatherForecastControllerTest.cs
@@ -0,0 +1,30 @@
+using Microsoft.Extensions.Logging;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Recep.Controllers;
+using System.Collections.Generic;
+
+namespace Recep.Tests;
+
+[TestClass]
+public class WeatherForecastControllerTest
+{
+    Mock<ILogger<WeatherForecastController>> mockLogger = new();
+
+
+    [TestInitialize]
+    public void BeforeEachTest()
+    {
+        mockLogger = new Mock<ILogger<WeatherForecastController>>();
+    }
+
+    [TestMethod]
+    public void GetTest()
+    {
+        WeatherForecastController controller = new WeatherForecastController(mockLogger.Object);
+
+        IEnumerable<WeatherForecast>? result = controller.Get();
+
+        Assert.IsNotNull(result);
+    }
+}
--- Recep/WeatherForecast.cs
+++ Recep/WeatherForecast.cs
@@ -1,5 +1,8 @@
+using System.Diagnostics.CodeAnalysis;
+
 namespace Recep;
 
+[ExcludeFromCodeCoverage]
 public class WeatherForecast
 {
     public DateTime Date { get; set; }
