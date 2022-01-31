764fae6dfc7e69911813a1f721c59f51a6aa02a7
Zhixian Ong <zhixian@hotmail.com>
2022-01-31 16:58:59 +0800

    update

--- Recep/Controllers/AuthenticationController.cs
+++ Recep/Controllers/AuthenticationController.cs
@@ -9,6 +9,7 @@ using Mini.Common.Settings;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Security.Cryptography;
+using System.Text.Json;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -34,6 +35,9 @@ public class AuthenticationController : ControllerBase
     [HttpPost]
     public OkObjectResult Post([FromBody] LoginRequest value)
     {
+
+        return Rsa(value);
+
         // Get Issuer and Audience
 
         var jwtSetting = jwtOptions.Get("Conso");
@@ -68,9 +72,12 @@ public class AuthenticationController : ControllerBase
             encryptingCredentials: new EncryptingCredentials(ecKey, SecurityAlgorithms.Aes256KW, SecurityAlgorithms.Aes256CbcHmacSha512)
             );
 
+        
+
         // For a list algorithms applicable to tokens see
         // See: https://datatracker.ietf.org/doc/html/rfc7518
-        // 
+        // See: https://github.com/AzureAD/azure-activedirectory-identitymodel-extensions-for-dotnet/wiki/Supported-Algorithms
+        //
 
         LoginResponse? res = new()
         {
@@ -80,5 +87,77 @@ public class AuthenticationController : ControllerBase
 
         return Ok(res);
 
+    }
+
+    public OkObjectResult Rsa(LoginRequest value)
+    {
+        // Get Issuer and Audience
+
+        var jwtSetting = jwtOptions.Get("Conso");
+        jwtSetting.EnsureIsValid();
+
+        // Setup claims
+
+        List<Claim>? authClaims = new()
+        {
+            // JwtRegisteredClaimNames contains a list of valid Jwt claim names
+
+            new Claim(ClaimTypes.Name, value.Username),
+            new Claim(ClaimTypes.Role, "Admin"),
+            new Claim(ClaimTypes.Role, "Developer"),
+            new Claim(ClaimTypes.Role, "Tester")
+        };
+
+        string secretKey = EnvironmentHelper.GetVariable(applicationSetting.Name);
+
+        (var scKey, var ecKey) = SecurityKeyHelper.SymmetricSecurityKey(secretKey, HashAlgorithmName.SHA256);
+
+        try
+        {
+            using (RSA myRsa = RSA.Create())
+            {
+                byte[] pvkBytes = System.IO.File.ReadAllBytes(@"D:\src\github\recep\test2.pvk");
+                
+                myRsa.ImportRSAPublicKey(pvkBytes, out int count);
+
+                var myRsaPbkParams = myRsa.ExportParameters(false);
+
+                AsymmetricSecurityKey key = new RsaSecurityKey(myRsaPbkParams);
+
+                AsymmetricSecurityKey signkey = new RsaSecurityKey(myRsa);
+
+                RsaSecurityKey signkey2;
+                using (var rsa = RSA.Create(2048))
+                {
+                    signkey2 = new RsaSecurityKey(rsa);
+                }
+
+                JwtSecurityTokenHandler? jwtSecurityTokenHandler = new();
+
+                var jwtSecurityToken = jwtSecurityTokenHandler.CreateJwtSecurityToken(
+                    issuer: jwtSetting.Issuer,
+                    audience: jwtSetting.Audience,
+                    subject: new ClaimsIdentity(authClaims),
+                    notBefore: DateTime.UtcNow,
+                    expires: DateTime.UtcNow.AddMinutes(jwtSetting.ExpirationMinutes),
+                    issuedAt: DateTime.UtcNow,
+                    //signingCredentials: new SigningCredentials(scKey, SecurityAlgorithms.HmacSha256, SecurityAlgorithms.Sha256Digest),
+                    signingCredentials: new SigningCredentials(signkey2, SecurityAlgorithms.RsaSsaPssSha256),
+                    encryptingCredentials: new EncryptingCredentials(key, SecurityAlgorithms.RsaOAEP, SecurityAlgorithms.Aes256CbcHmacSha512)
+                );
+
+                LoginResponse? res = new()
+                {
+                    Jwt = jwtSecurityTokenHandler.WriteToken(jwtSecurityToken),
+                    ExpiryDateTime = jwtSecurityToken.ValidTo
+                };
+
+                return Ok(res);
+            }
+        }
+        catch (Exception ex)
+        {
+            throw;
+        }
     }
 }
