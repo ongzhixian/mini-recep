5c882cc2b9fe45808f5a5481bb8a0b7e51ebd0ce
zhixian <zhixian@hotmail.com>
2022-02-02 23:36:17 +0800

    update

---
+++ Recep/Models/Customer.cs
@@ -0,0 +1,43 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace Recep.Models;
+
+public interface ICustomer
+{
+    string Id { get; set; }
+    
+    string Name { get; set; }
+
+    DateTime DateCreated { get; set; }
+
+}
+
+public class Customer : ICustomer
+{
+    [Key]
+    [Required]
+    public string Id { get; set; } = string.Empty;
+
+    [Required]
+    public string Name { get; set; } = string.Empty;
+
+    //[JsonPropertyName("Wind")]
+    public DateTime DateCreated { get; set; } = DateTime.UtcNow;
+
+    //[BsonId]
+    //[BsonRepresentation(BsonType.ObjectId)]
+    //public string Id { get; set; } = string.Empty;
+
+    //public string Title { get; set; } = string.Empty;
+
+    //[BsonElement("content")]
+    //public string Content { get; set; } = string.Empty;
+
+    //[BsonElement("date_created")]
+    //public DateTime DateCreated { get; set; } = DateTime.UtcNow;
+
+    //[BsonElement("tags")]
+    //public string[] Tags { get; set; } = new string[] { };
+
+}
---
+++ Recep/Services/CustomerService.cs
@@ -0,0 +1,37 @@
+﻿using Recep.Models;
+
+// namespace Recep.Wms.Services;
+
+namespace Recep.Services;
+
+public interface ICustomerService
+{
+    IList<ICustomer> GetCustomerList();
+
+    void Add(ICustomer customer);
+
+    void Update(ICustomer customer);
+
+    //void Archive(ICustomer customer);
+
+    //void Remove(ICustomer customer);
+
+}
+
+public class CustomerService : ICustomerService
+{
+    public IList<ICustomer> GetCustomerList()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Add(ICustomer customer)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Update(ICustomer customer)
+    {
+        throw new NotImplementedException();
+    }
+}
