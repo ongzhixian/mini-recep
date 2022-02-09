448649d6ba3e558e0f65f82d2f01fcf1cbea2aa1
Zhixian Ong <zhixian@hotmail.com>
2022-02-09 17:53:14 +0800

    temp update

--- Recep/Controllers/UserController.cs
+++ Recep/Controllers/UserController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Mini.Wms.Abstraction.Models;
 using Mini.Wms.Abstraction.Services;
 using Mini.Wms.DomainMessages;
@@ -10,7 +11,7 @@ using Recep.Models;
 
 namespace Recep.Controllers;
 
-
+[AllowAnonymous]
 [Route("api/[controller]")]
 [ApiController]
 public class UserController : ControllerBase
